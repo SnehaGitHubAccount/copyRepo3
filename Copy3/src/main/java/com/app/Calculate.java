3624243529c8475a7b8da96556881a4d959cd017
sneha <sneha.wanare@vyomlabs.com>
2019-01-08 14:31:51 +0530

    commit4

--- Copy3/src/main/java/com/app/Calculate.java
+++ Copy3/src/main/java/com/app/Calculate.java
@@ -65,16 +65,16 @@ public class Calculate extends HttpServlet {
 			  System.out.println("In try");
 				
 
-				String insertTableSQL = "INSERT INTO product.`product_entry` (product_name,quantity,total_cost) VALUES(?,?,?)";
+				String insertTableSQL = "INSERT INTO product.product_entry (product_name,quantity,total_cost) VALUES(?,?,?)";
 				PreparedStatement preparedStatement = con.prepareStatement(insertTableSQL);
 				
 				for (int i=0; i<products.length; i++)
 				{
 					System.out.println(products[i] + " " + quantites[i]);
 					
-					preparedStatement.setString(2, products[i]);
-					preparedStatement.setInt(3, Integer.parseInt(quantites[i]));
-					preparedStatement.setInt(4,Integer.parseInt(totalamount[i]));
+					preparedStatement.setString(1, products[i]);
+					preparedStatement.setInt(2, Integer.parseInt(quantites[i]));
+					preparedStatement.setInt(3,Integer.parseInt(totalamount[i]));
 					preparedStatement.addBatch();
 				}
 				int[] aa=preparedStatement.executeBatch();
